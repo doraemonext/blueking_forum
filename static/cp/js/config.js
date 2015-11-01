1e3a89ef3074803ee0f304d88c0ca7ffd0ded010
doraemonext <doraemonext@gmail.com>
2015-11-01 20:59:33 +0800

    Add: 添加插件

---
+++ static/cp/js/config.js
@@ -0,0 +1,40 @@
+require.config({
+    baseUrl: '/static/cp/js',
+    shim: {
+        'underscore': { 'exports': '_' },
+        'backbone-paginator': { 'deps': ['backbone'] },
+        'bootstrap': { 'deps': ['jquery'] },
+        'jquery-cookie': { 'deps': ['jquery'] },
+        'noty': { 'deps': ['jquery'] },
+        'jquery-validate': { 'deps': ['jquery'] },
+        'jquery-form': { 'deps': ['jquery'] },
+        'spin': { 'exports': 'Spinner' },
+        'ckeditor': { 'exports': 'CKEDITOR' },
+        'theme-app': { 'deps': ['jquery', 'bootstrap'] }
+    },
+    paths: {
+        'jquery': 'plugins/jquery/jquery.min',
+        'underscore': 'plugins/underscore/underscore.min',
+        'backbone': 'plugins/backbone/backbone.min',
+
+        'backbone-paginator': 'plugins/backbone/backbone.paginator.min',
+        'bootstrap': 'plugins/bootstrap/bootstrap.min',
+        'jquery-cookie': 'plugins/jquerycookie/jquery.cookie',
+        'jquery-validate': 'plugins/jqueryvalidate/jquery.validate.min',
+        'jquery-form': 'plugins/jqueryform/jquery.form',
+        'noty': 'plugins/noty/jquery.noty.packaged.min',
+        'spin': 'plugins/spin/spin.min',
+        'ckeditor': 'plugins/ckeditor/ckeditor',
+        'common': 'common',
+/*
+        'module.common.app-view': 'module/common/app_view',
+        'module.common.app-breadcrumb-view': 'module/common/app_breadcrumb_view',
+        'module.common.app-content-header-view': 'module/common/app_content_header_view',
+        'module.official-account': 'module/official_account',
+        'module.library.music': 'module/library/music',
+        'module.library.news': 'module/library/news',
+
+        'helper.confirm-modal': 'helper/confirm_modal/confirm_modal'
+*/
+    }
+});
